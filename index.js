20f5b82e4aa3a7fd32fb70e62f49a52a51f4c050
keller35 <524495217@qq.com>
2016-04-23 19:05:42 +0800

    add api for validate signature

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ const CONFIG = {
     debug: false
 };
 
-function MEITUAN(appId, secret, config) {
+function Meituan(appId, secret, config) {
     this.appId = appId;
     this.secret = secret;
     this.configuration = Object.assign(CONFIG, config);
 }
 
-MEITUAN.prototype.config = function (config) {
+Meituan.prototype.config = function(config) {
     this.configuration = Object.assign(this.configuration, config);
 }
 
-MEITUAN.prototype.getHost = function () {
+Meituan.prototype.getHost = function() {
     return this.configuration.debug ? this.configuration.testHost : this.configuration.realHost;
 }
 
-MEITUAN.prototype.sign = function (url, params) {
+Meituan.prototype.sign = function(url, params) {
     params.timestamp = moment().unix();
     params.app_id = this.appId;
     const rawString = [
@@ -40,7 +40,24 @@ MEITUAN.prototype.sign = function (url, params) {
     return params;
 }
 
-MEITUAN.prototype.get = function (path, params) {
+Meituan.prototype.validateSign = function(url, params) {
+    let copy_params = Object.assign({}, params);
+    const sig = copy_params.sig;
+    delete copy_params.sig;
+    const rawString = [
+        url,
+        '?',
+        Object.keys(copy_params).sort().map(key => key + '=' + params[key]).join('&'),
+        this.secret
+    ].join('');
+    return crypto
+        .createHash('md5')
+        .update(rawString, 'utf8')
+        .digest('hex')
+        .toLowerCase() === sig;
+}
+
+Meituan.prototype.get = function(path, params) {
     let url = this.getHost() + path;
     this.sign(url, params);
     return rp({
@@ -50,7 +67,7 @@ MEITUAN.prototype.get = function (path, params) {
     });
 }
 
-MEITUAN.prototype.post = function (path, params) {
+Meituan.prototype.post = function(path, params) {
     let url = this.getHost() + path;
     this.sign(url, params);
     return rp({
@@ -61,4 +78,4 @@ MEITUAN.prototype.post = function (path, params) {
     });
 }
 
-module.exports = MEITUAN;
+module.exports = Meituan;
